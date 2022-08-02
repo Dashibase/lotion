6b4b35c8839e790b1fd47f45d5f783fc6dc1d1d0
SK <limsweekiat@gmail.com>
2022-08-03 02:18:54 +0800

    Update styles

--- src/components/elements/Editor.vue
+++ src/components/elements/Editor.vue
@@ -49,9 +49,3 @@ watch(() => props.modelValue, value => {
   editor.value?.commands.setContent(value, false)
 })
 </script>
-
-<style scoped>
-:deep([contenteditable="true"]:focus) {
-  outline: none;
-}
-</style>
