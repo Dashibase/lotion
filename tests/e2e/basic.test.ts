dcef7927054f805dd12f8476b28ca9918b260dcd
SK <limsweekiat@gmail.com>
2022-08-19 14:38:01 +0800

    Check that tests fail correctly in Actions

--- tests/e2e/basic.test.ts
+++ tests/e2e/basic.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test'
 
 test('basic tests', async ({ page }) => {
   await page.goto('/')
-  await expect(page).toHaveTitle(/Lotion/)
+  await expect(page).toHaveTitle(/Potion/)
 })
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -4,11 +4,11 @@ import { test, expect } from 'vitest'
 test('isTextBlock should work for text blocks', () => {
   // Keep as reminder to update textBlockMap in @/utils/types
   const knownTextBlocks = [
-    BlockType.Text,
+    // BlockType.Text,
     BlockType.Quote,
-  ] 
-  for (let type in BlockType) {
+  ] as any[]
+  Object.values(BlockType).forEach(type => {
     if (knownTextBlocks.includes(type as BlockType)) expect(isTextBlock(type)).toBeTruthy()
     else expect(isTextBlock(type)).toBeFalsy()
-  }
+  })
 })
