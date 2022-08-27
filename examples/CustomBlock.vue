1c0258e6b61112a7fbafe1d937d681987ca3a32b
SK <limsweekiat@gmail.com>
2022-08-27 19:59:17 +0800

    Fix example for custom block

--- examples/CustomBlock.vue
+++ examples/CustomBlock.vue
@@ -12,12 +12,21 @@ const props = defineProps({
     type: Object as PropType<types.Block>,
     required: true,
   },
+  readonly: {
+    type: Boolean,
+    default: false,
+  },
 })
 
+// This is triggered when a user converts any block into this block
+// It is called before the blocktype is changed
 function onSet () {
   alert('Moisturizing...')
 }
 
+
+// This is triggered when a user converts this block into any other block
+// It is called before the blocktype is changed
 function onUnset () {
   alert('Moisturized!')
 }
