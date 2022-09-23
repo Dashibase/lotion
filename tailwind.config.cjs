23165777f0a53bf0e2bcab338de2900346c1d4bf
noe <826547343@qq.com>
2022-09-23 08:57:18 +0800

    fix: the Read-only button has a lower zindex value

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,28 +2,28 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const plugin = require('tailwindcss/plugin')
 
-
 module.exports = {
   prefix: '',
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       boxShadow: {
-        'block': 'rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px',
+        block: 'rgba(15, 15, 15, 0.05) 0px 0px 0px 1px, rgba(15, 15, 15, 0.1) 0px 3px 6px, rgba(15, 15, 15, 0.2) 0px 9px 24px',
       },
       borderWidth: {
         DEFAULT: '1px',
-        '0': '0',
-        '2': '2px',
-        '3': '3px',
-        '4': '4px',
-      }
+        0: '0',
+        2: '2px',
+        3: '3px',
+        4: '4px',
+      },
+    },
+    zIndex: {
+      999: 999,
+      auto: 'auto',
     },
     groups: ['tooltip'],
   },
@@ -39,6 +39,6 @@ module.exports = {
           return `:merge(.group-${group}):hover &`
         })
       })
-    })
-  ]
+    }),
+  ],
 }
